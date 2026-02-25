1f61da025b477475c7dde82ce097866133563de4
dionteallen <dionteallencomedy@gmail.com>
2026-02-25 18:06:19 -0500

    Delete src/lib/src/lib/src/lib/adminGuard.ts

--- src/lib/src/lib/src/lib/adminGuard.ts
+++
@@ -1,7 +0,0 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-
-export function requireAdmin() {
-  const isAdmin = cookies().get("fatw_admin")?.value === "1";
-  if (!isAdmin) redirect("/access");
-}
